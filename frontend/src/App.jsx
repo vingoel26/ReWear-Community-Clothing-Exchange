ffac0c87ae6035a98c149fdbc98fbdfcfce42357
vingoel26 <vinayakgoel012@gmail.com>
2025-07-12 11:05:03 +0530

    frontend setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 class="text-l font-bold underline ">Hello world!</h1>
+    </>
+  );
+}
+
+export default App;
